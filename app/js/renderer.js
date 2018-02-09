fcfdbb8abab641bd7901308f2fee89c218fd17ae
Juliano Pereira Lima <julianogutierrez@hotmail.com>
2018-02-09 17:38:50 -0200

    :star: inserts the alternation of display on button play

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,8 +1,15 @@
 const { ipcRenderer } = require('electron');
 
 let linkSobre = document.querySelector('#link-sobre');
-
+let botaoPlay = document.querySelector('.botao-play');
 
 linkSobre.addEventListener('click' , function(){
     ipcRenderer.send('abrir-janela-sobre');
+});
+
+let imgs = ['img/play-button.svg','img/stop-button.svg'];
+
+botaoPlay.addEventListener('click',()=>{
+    imgs = imgs.reverse();
+    botaoPlay.src= imgs[0];
 });
