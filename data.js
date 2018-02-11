033eff36a7fc900c3aad7c4d0725c62979c28215
JulianoGTZ <julianogutierrez@hotmail.com>
2018-02-11 02:07:41 -0200

    :star: Adds a function that returns the registered courses

--- data.js
+++ data.js
@@ -7,10 +7,9 @@ module.exports = {
         let enderecoArquivoTempoGastoCurso = __dirname + '/data/' + curso + '.json';
 
         if (fs.existsSync(enderecoArquivoTempoGastoCurso)) {
-            this.adicionaTempoAoCurso(enderecoArquivoTempoGastoCurso,tempoCursoEstudado);
+            this.adicionaTempoAoCurso(enderecoArquivoTempoGastoCurso, tempoCursoEstudado);
         } else {
-            this.criaArquivoCurso(enderecoArquivoTempoGastoCurso, {}).
-            then(() => {
+            this.criaArquivoCurso(enderecoArquivoTempoGastoCurso, {}).then(() => {
                 this.adicionaTempoAoCurso(enderecoArquivoTempoGastoCurso, tempoCursoEstudado)
             })
         }
@@ -30,11 +29,18 @@ module.exports = {
         jsonfile.writeFile(arquivoCurso, dados, {spaces: 2}).then(() => {
             console.log('tempo salvo com sucesso no arquivo');
         }).catch((error) => {
-            console.log('aconteceu o seguinte erro',error);
+            console.log('aconteceu o seguinte erro', error);
         });
     },
-    getDadosCurso(nomeCurso){
+    getDadosCurso(nomeCurso) {
         let caminhoArquivoCurso = __dirname + '/data/' + nomeCurso + '.json';
         return jsonfile.readFile(caminhoArquivoCurso)
+    },
+    getNomesCursos() {
+        let arquivos = fs.readdirSync(__dirname + '/data/');
+        let cursos = arquivos.map((arquivo) => {
+            return arquivo.substr(0, arquivo.lastIndexOf('.'));
+        });
+        return cursos;
     }
 };
