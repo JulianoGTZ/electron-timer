bca1f0631c32b686e716215fc7749132856b65cc
Juliano Pereira Lima <julianodgtz@gmail.com>
2019-08-06 20:10:02 -0300

    :globe_with_meridians: Rename var on timer.js

--- app/js/timer.js
+++ app/js/timer.js
@@ -1,27 +1,30 @@
-const {ipcRenderer} = require('electron');
-const moment = require('moment');
+const { ipcRenderer } = require("electron");
+const moment = require("moment");
 let seconds = 0;
 let timer;
 let time;
 
 module.exports = {
-    start(htmlElement) {
-        time = moment.duration(htmlElement.textContent);
-        seconds = time.asSeconds();
-        clearInterval(timer);
-        timer = setInterval(() => {
-            seconds++;
-            time = seconds;
-            htmlElement.textContent = this.secondsToFormatedDate(seconds);
-        }, 1000);
-    },
-    secondsToFormatedDate(segundos) {
-        return moment().startOf('day').seconds(segundos).format("HH:mm:ss");
-    },
-    stop(courseName) {
-        clearInterval(timer);
-        let timeStudied = this.secondsToFormatedDate(time);
-        console.log('time studied.',timeStudied);
-        ipcRenderer.send('stopped-course', courseName, timeStudied);
-    }
+  start(htmlElement) {
+    time = moment.duration(htmlElement.textContent);
+    seconds = time.asSeconds();
+    clearInterval(timer);
+    timer = setInterval(() => {
+      seconds++;
+      time = seconds;
+      htmlElement.textContent = this.secondsToFormatedDate(seconds);
+    }, 1000);
+  },
+  secondsToFormatedDate(seconds) {
+    return moment()
+      .startOf("day")
+      .seconds(seconds)
+      .format("HH:mm:ss");
+  },
+  stop(courseName) {
+    clearInterval(timer);
+    let timeStudied = this.secondsToFormatedDate(time);
+    console.log("time studied.", timeStudied);
+    ipcRenderer.send("stopped-course", courseName, timeStudied);
+  }
 };
