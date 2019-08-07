1e7a50a6c7dc32cbf287155568ce2f4b1a8703b6
Juliano Pereira Lima <julianodgtz@gmail.com>
2019-08-06 21:07:23 -0300

    :globe_with_meridians: rename methods on template file

--- template.js
+++ template.js
@@ -1,82 +1,83 @@
-const data = require('./data');
-const {ipcMain} = require('electron');
+const data = require("./data");
+const { ipcMain } = require("electron");
 
 module.exports = {
-    templateInicial: null,
+  initialTemplate: null,
 
-    geraTrayTemplate(win) {
-        let template = [
-            {
-                'label': 'Cursos'
-            },
-            {
-                type: 'separator'
-            }
-        ];
+  generateTrayTemplate(win) {
+    let template = [
+      {
+        label: "Courses"
+      },
+      {
+        type: "separator"
+      }
+    ];
 
-        let cursos = data.getNomesCursos();
+    let courses = data.getCourseNames();
 
-        cursos.forEach((curso) => {
-            let menuItem = {
-                label: curso,
-                type: 'radio',
-                click: () => {
-                    win.send('exchanged-course', curso);
-                }
-            };
-            template.push(menuItem);
-        });
-        this.templateInicial = template;
-        return template;
-    },
-    adicionaCursoNoTray(curso, win) {
-        this.templateInicial.push({
-            label: curso,
-            type: 'radio',
-            checked: true,
+    courses.forEach(course => {
+      let menuItem = {
+        label: course,
+        type: "radio",
+        click: () => {
+          win.send("exchanged-course", course);
+        }
+      };
+      template.push(menuItem);
+    });
+    this.templateInicial = template;
+    return template;
+  },
+  addCourseOnTray(course, win) {
+    this.initialTemplate.push({
+      label: course,
+      type: "radio",
+      checked: true,
+      click: () => {
+        win.send("exchanged-course", course);
+      }
+    });
+
+    return this.initialTemplate;
+  },
+  generateMainMenuTemplate(app) {
+    let templateMenu = [
+      {
+        label: "View",
+        submenu: [
+          {
+            role: "reload"
+          },
+          {
+            role: "toggledevtools"
+          }
+        ]
+      },
+      {
+        label: "Window",
+        submenu: [
+          {
+            role: "minimize"
+          },
+          {
+            role: "close"
+          }
+        ]
+      },
+      {
+        label: "About",
+        submenu: [
+          {
+            label: "About Electron Timer",
             click: () => {
-                win.send('exchanged-course', curso);
-            }
-        });
-
-        return this.templateInicial;
-    },
-    geraMenuPrincipalTemplate(app) {
-        let templateMenu = [
-            {
-                label: 'View',
-                submenu: [{
-                    role: 'reload'
-                },
-                    {
-                        role: 'toggledevtools'
-                    }]
+              ipcMain.emit("open-window-about");
             },
-            {
-                label: 'Window',
-                submenu: [
-                    {
-                        role: 'minimize'
-                    },
-                    {
-                        role: 'close'
-                    }
-                ]
-            },
-            {
-                label: 'Sobre',
-                submenu: [
-                    {
-                        label: 'About Electron Timer',
-                        click: () => {
-                            ipcMain.emit('open-window-about');
-                        },
-                        accelerator: 'CommandOrControl+I'
-                    }
-                ]
-            }
-        ];
-        return templateMenu;
-    }
-
+            accelerator: "CommandOrControl+I"
+          }
+        ]
+      }
+    ];
+    return templateMenu;
+  }
 };
